b1cec1a0b9d506190724e854ae82db7b3d676e5f
dylan <dylan.ananda@gmail.com>
2025-08-22 00:18:56 +0800

    Create Message class

---
+++ src/main/java/Message.java
@@ -0,0 +1,18 @@
+import java.util.Objects;
+
+public class Message {
+    public static String sendMessage(String input) {
+        if (Objects.equals(input, "bye")) {
+            return """
+                    ____________________________________________________________
+                    Bye, see you around!
+                    ____________________________________________________________
+                    """;
+        }
+        else {
+            return "____________________________________________________________\n" +
+                    input + "\n" +
+                    "____________________________________________________________";
+        }
+    }
+}
