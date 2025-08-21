57a5260f3e9caa3e5630372cb5b81195132594cb
dylan <dylan.ananda@gmail.com>
2025-08-22 03:02:16 +0800

    Add Maximum length checks and try catch block

--- src/main/java/Wheezy.java
+++ src/main/java/Wheezy.java
@@ -19,57 +19,66 @@ public class Wheezy {
         System.out.println("        ____________________________________________________________\n");
 
         while (true) {
-            String input = scanner.nextLine();
-            CommandType commandType = CommandParser.parseCommand(input);
+            try {
+                String input = scanner.nextLine();
+                CommandType commandType = CommandParser.parseCommand(input);
 
-            switch (commandType) {
-                case BYE -> {
-                    System.out.println(Message.byeMessage());
+                switch (commandType) {
+                    case BYE -> {
+                        System.out.println(Message.byeMessage());
+                        break;
+                    }
+                    case LIST -> {
+                        System.out.println(Message.listMessage(list, counter));
+                    }
+                    case MARK -> {
+                        CommandHandler.handleMark(input, list, counter, true);
+                    }
+                    case UNMARK -> {
+                        CommandHandler.handleMark(input, list, counter, false);
+                    }
+                    case ADD_TASK -> {
+                        if (counter >= 100) {
+                            ErrorHandler.printError("Oops! Your task list is full. Cannot add more tasks.");
+                        }
+                        counter++;
+                        CommandHandler.handleAdd(input, list, counter);
+                    }
+                    case TODO -> {
+                        if (counter >= 100) {
+                            ErrorHandler.printError("Oops! Your task list is full. Cannot add more tasks.");
+                        }
+                        counter++;
+                        String description = CommandParser.extractTodoDescription(input);
+                        CommandHandler.handleTodo(description, list, counter);
+                    }
+                    case DEADLINE -> {
+                        if (counter >= 100) {
+                            ErrorHandler.printError("Oops! Your task list is full. Cannot add more tasks.");
+                        }
+                        counter++;
+                        String description = CommandParser.extractDeadlineDescription(input);
+                        String deadline = CommandParser.extractDeadlineDate(input);
+                        CommandHandler.handleDeadline(description, list, counter, deadline);
+                    }
+                    case EVENT -> {
+                        counter++;
+                        String description = CommandParser.extractEventDescription(input);
+                        String from = CommandParser.extractEventStartTime(input);
+                        String until = CommandParser.extractEventEndTime(input);
+                        CommandHandler.handleEvent(description, list, counter, from, until);
+                    }
+                    case INVALID -> {
+                        ErrorHandler.printError("I don't understand that command. Try 'list', 'todo <description>', or 'bye'.");
+                    }
+
+                }
+
+                if (commandType == CommandType.BYE) {
                     break;
                 }
-                case LIST -> {
-                    System.out.println(Message.listMessage(list, counter));
-                }
-                case MARK -> {
-                    CommandHandler.handleMark(input, list, counter, true);
-                }
-                case UNMARK -> {
-                    CommandHandler.handleMark(input, list, counter, false);
-                }
-                case ADD_TASK -> {
-                    if (counter >= 100) {
-                        printError("Oops! Your task list is full. Cannot add more tasks.");
-                    }
-                    counter++;
-                    CommandHandler.handleAdd(input, list, counter);
-                }
-                case TODO -> {
-                    counter++;
-                    String description = CommandParser.extractTodoDescription(input);
-                    CommandHandler.handleTodo(description, list, counter);
-                }
-                case DEADLINE -> {
-                    counter++;
-                    String description = CommandParser.extractDeadlineDescription(input);
-                    String deadline = CommandParser.extractDeadlineDate(input);
-                    CommandHandler.handleDeadline(description, list, counter, deadline);
-                }
-                case EVENT -> {
-                    counter++;
-                    String description = CommandParser.extractEventDescription(input);
-                    String from = CommandParser.extractEventStartTime(input);
-                    String until = CommandParser.extractEventEndTime(input);
-                    CommandHandler.handleEvent(description, list, counter, from, until);
-                }
-                case INVALID -> {
-                    System.out.println("        ____________________________________________________________");
-                    System.out.println("        Invalid Command");
-                    System.out.println("        ____________________________________________________________");
-                }
-            }
-
-            if (commandType == CommandType.BYE) {
-                break;
+            } catch (Exception e) {
+                ErrorHandler.printError("Something went wrong! Please try again");
             }
         }
     }
