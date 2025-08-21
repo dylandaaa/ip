3ea586771de6997b4727f1eaf9c4f3ef5b363c06
dylan <dylan.ananda@gmail.com>
2025-08-21 23:58:36 +0800

    Change bot name from Duke to Wheezy

---
+++ src/main/java/Wheezy.java
@@ -0,0 +1,13 @@
+public class Wheezy {
+    public static void main(String[] args) {
+        String logo = """
+                 __      __.__                                \s
+                /  \\    /  \\  |__   ____   ____ ___________.__.
+                \\   \\/\\/   /  |  \\_/ __ \\_/ __ \\\\___   |   |  |
+                 \\        /|   Y  \\  ___/\\  ___/ /    /_\\___  |
+                  \\__/\\__/ |___|__/\\____/ \\____/ |_____| /____|
+                """;
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
