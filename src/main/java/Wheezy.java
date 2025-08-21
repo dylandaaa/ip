1d26df4e1e8e327ef96c2b79f51acaf2b50c0ac2
dylan <dylan.ananda@gmail.com>
2025-08-22 00:03:24 +0800

    Add lines and edit message

--- src/main/java/Wheezy.java
+++ src/main/java/Wheezy.java
@@ -7,7 +7,11 @@ public class Wheezy {
                  \\        /|   Y  \\  ___/\\  ___/ /    /_\\___  |
                   \\__/\\__/ |___|__/\\____/ \\____/ |_____| /____|
                 """;
+        System.out.println("____________________________________________________________");
         System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
