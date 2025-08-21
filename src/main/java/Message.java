934cd908fbe08f81cd2bcec7df41aa4232a64368
dylan <dylan.ananda@gmail.com>
2025-08-22 01:47:28 +0800

    Add separate functions for commands

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,26 +1,25 @@
 public class Message {
-    public static String sendMessage(String input, Task[] list, int counter) {
-        if (input.equals("bye")) {
-            return """
+    public static String byeMessage() {
+        return """
                             ____________________________________________________________
                             Bye, see you around!
                             ____________________________________________________________
                     """;
-        } else if (!input.equals("list")) {
-            return "       ____________________________________________________________\n" +
-                    "       added: " + input + "\n" +
-                    "       ____________________________________________________________\n";
-        } else {
-            StringBuilder message = new StringBuilder("       ____________________________________________________________\n");
+    }
 
-            for (int i = 0; i < counter; i++) {
-                message.append((i + 1))
-                        .append(". ")
-                        .append(list[i])
-                        .append("\n");
-            }
-            message.append("       ____________________________________________________________\n");
-            return message.toString();
+    public static String listMessage(Task[] list, int counter) {
+        StringBuilder message = new StringBuilder();
+        message.append("       ____________________________________________________________\n");
+        for (int i = 0; i < counter; i++) {
+            message.append((i + 1))
+                    .append(list[i]);
         }
+        return message.toString();
+    }
+
+    public static String addMessage(String input) {
+        return "        ____________________________________________________________\n" +
+                "       " + input + "\n" +
+                "       ____________________________________________________________\n";
     }
 }
