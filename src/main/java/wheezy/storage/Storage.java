46b700e50a1f818d55e3fb81cace503d99adcec3
dylan <dylan.ananda@gmail.com>
2025-08-28 00:46:58 +0800

    Add JavaDoc for CommandType and Storage class

--- src/main/java/wheezy/storage/Storage.java
+++ src/main/java/wheezy/storage/Storage.java
@@ -17,12 +17,29 @@ import wheezy.task.Deadline;
 import wheezy.task.Event;
 import wheezy.task.Todo;
 
+/*
+ * Used to handle things related to file I/O. Used to parse string input
+ * in the storage format to tasks.
+ */
 public class Storage {
+    /**
+     * Creates the necessary file and directory to save task data.
+     *
+     * @throws IOException If file corrupts while creating.
+     */
     public static void createDirectory() throws IOException {
         new File("data").mkdirs();
         new File("data/wheezy.txt").createNewFile();
     }
 
+    /**
+     * Reads the contents of the data file and parses it via fileContentParser.
+     * Adds the tasks to a TaskList.
+     *
+     * @param taskList Empty TaskList.
+     * @return TaskList containing the tasks read from the file.
+     * @throws FileNotFoundException If storage file is not found.
+     */
     public static TaskList loadContent(TaskList taskList) throws FileNotFoundException {
         File f = new File("data/wheezy.txt");
         Scanner scanner = new Scanner(f);
@@ -35,6 +52,14 @@ public class Storage {
         return taskList;
     }
 
+    /**
+     * Marks/unmarks the specified task in the correct file format.
+     * Creates a new data file in the process.
+     *
+     * @param taskNumber Integer representing the task number (1 indexing).
+     * @param isDone Boolean representing to mark or unmark the task.
+     * @throws IOException If storage file is not found.
+     */
     public static void fileMark(int taskNumber, boolean isDone) throws IOException {
         File f = new File("data/wheezy.txt");
         List<String> lines = new ArrayList<>();
@@ -63,12 +88,24 @@ public class Storage {
         }
     }
 
+    /**
+     * Adds a line in the storage file when a new task is added.
+     *
+     * @param task New task to add to the file.
+     * @throws IOException If storage file is not found.
+     */
     public static void fileAdd(Task task) throws IOException {
         FileWriter fw = new FileWriter("data/wheezy.txt", true);
         fw.write(task.toFileString() + "\n");
         fw.close();
     }
 
+    /**
+     * Deletes the specified task in the storage file.
+     *
+     * @param taskNumber Integer to represent the task number to delete (1 indexing).
+     * @throws IOException If storage file is not found.
+     */
     public static void fileDelete(int taskNumber) throws IOException {
         File f = new File("data/wheezy.txt");
         List<String> lines = new ArrayList<>();
@@ -90,6 +127,14 @@ public class Storage {
         }
     }
 
+    /**
+     * Parses the line from the storage format:
+     * <tasktype>|<isdone>|<description>|<optional-dates>
+     * The appropriate task type is then created from the parsed line.
+     *
+     * @param input String containing a line in the above file format.
+     * @return Task of the appropriate type.
+     */
     public static Task fileContentParser(String input) {
         String[] parts = input.split("\\|");
         String description = parts[2];
