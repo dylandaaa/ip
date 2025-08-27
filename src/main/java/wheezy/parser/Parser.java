afd2212b1121d3f890576fbeeafc64a665008879
dylan <dylan.ananda@gmail.com>
2025-08-28 00:21:55 +0800

    Add JavaDoc for Parser class

--- src/main/java/wheezy/parser/Parser.java
+++ src/main/java/wheezy/parser/Parser.java
@@ -2,7 +2,20 @@ package wheezy.parser;
 
 import wheezy.commandtype.CommandType;
 
+/*
+ * Used for manipulating string input. Used when trying to extract the command
+ * type of the user input. Other methods are used to extract important
+ * information from the string input such as description, dates, and task
+ * number.
+ */
 public class Parser {
+    /**
+     * Returns command type of user input.
+     * If the input is invalid, returns an invalid command type.
+     *
+     * @param input User input typed from the terminal.
+     * @return Command type of the input.
+     */
     public static CommandType parseCommand(String input) {
         String command = input.trim().toLowerCase();
 
@@ -34,6 +47,13 @@ public class Parser {
         return CommandType.INVALID;
     }
 
+    /**
+     * Extracts the task number from inputs of the format <command> <number>.
+     *
+     * @param input User input typed in the terminal in the above format.
+     * @return Task number specified in the input.
+     * @throws NumberFormatException If format is invalid.
+     */
     public static int extractTaskNumber(String input) throws NumberFormatException {
         // taskNumber returns in 0 format
         String[] parts = input.split(" ");
@@ -41,10 +61,22 @@ public class Parser {
         return Integer.parseInt(parts[1]) - 1;
     }
 
+    /**
+     * Extracts the description of todo tasks.
+     *
+     * @param input User input that involves the todo command.
+     * @return Description of the task.
+     */
     public static String extractTodoDescription(String input) {
         return input.substring(5).trim();
     }
 
+    /**
+     * Extracts the description of deadline tasks.
+     *
+     * @param input User input that involves the deadline command.
+     * @return Description of the task.
+     */
     public static String extractDeadlineDescription(String input) {
         String withoutCommand = input.substring(9).trim();
         int byIndex = withoutCommand.indexOf("/by ");
@@ -54,6 +86,12 @@ public class Parser {
         return withoutCommand.substring(0, byIndex).trim();
     }
 
+    /**
+     * Extracts the date of deadline tasks.
+     *
+     * @param input User input that involves the deadline command.
+     * @return Deadline of the task.
+     */
     public static String extractDeadlineDate(String input) {
         String withoutCommand = input.substring(9).trim();
         int byIndex = withoutCommand.indexOf("/by ");
@@ -63,6 +101,12 @@ public class Parser {
         return withoutCommand.substring(byIndex + 4).trim(); // Get everything after "/by "
     }
 
+    /**
+     * Extracts the description of event tasks.
+     *
+     * @param input User input that involves the event command.
+     * @return Description of the event.
+     */
     public static String extractEventDescription(String input) {
         String withoutCommand = input.substring(6).trim();
         int fromIndex = withoutCommand.indexOf("/from ");
@@ -72,6 +116,12 @@ public class Parser {
         return withoutCommand.substring(0, fromIndex).trim();
     }
 
+    /**
+     * Extracts the start time of event tasks.
+     *
+     * @param input User input that involves the event command.
+     * @return Start time of the event.
+     */
     public static String extractEventStartTime(String input) {
         String withoutCommand = input.substring(6).trim();
         int fromIndex = withoutCommand.indexOf("/from ");
@@ -84,6 +134,12 @@ public class Parser {
         return withoutCommand.substring(fromIndex + 6, toIndex).trim();
     }
 
+    /**
+     * Extracts the end time of event tasks.
+     *
+     * @param input User input that involves the event command.
+     * @return End time of the event.
+     */
     public static String extractEventEndTime(String input) {
         String withoutCommand = input.substring(6).trim();
         int toIndex = withoutCommand.indexOf("/to ");
